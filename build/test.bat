7eaf320912a17698f4e2e30583ad1169f6a15652
Viko <viko@vikomprenas.com>
2015-07-22 11:12:32 -0600

    Added tests to test.bat

--- build/test.bat
+++ build/test.bat
@@ -1,2 +1,7 @@
 @echo off
-echo No tests implemented yet.
+echo ==== Hello World LBCL
+export\Main lbcl examples\HelloWorld.lbcl.txt
+echo ==== Hello World HBCL
+export\Main hbcl examples\HelloWorld.hbcl.txt
+echo ==== Guess the Number HBCL (the number is 36)
+export\Main hbcl examples\GuessNumber.hbcl.txt
