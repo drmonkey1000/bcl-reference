ace9ea1f1f466795295e02fde18f9f213b159e7d
Viko <viko@vikomprenas.com>
2015-07-22 11:32:25 -0600

    Expanded test script [ci skip]

--- build/test.bat
+++ build/test.bat
@@ -4,4 +4,11 @@ export\Main lbcl examples\HelloWorld.lbcl.txt
 echo ==== Hello World HBCL
 export\Main hbcl examples\HelloWorld.hbcl.txt
 echo ==== Guess the Number HBCL (the number is 36)
-export\Main hbcl examples\GuessNumber.hbcl.txt
+echo == First try (correct)
+echo 36| export\Main hbcl examples\GuessNumber.hbcl.txt
+echo == Second try (incorrect, 46)
+echo 46| export\Main hbcl examples\GuessNumber.hbcl.txt
+echo == Third try (incorrect, 31)
+echo 31| export\Main hbcl examples\GuessNumber.hbcl.txt
+echo == Fourth try (incorrect, 149)
+echo 149| export\Main hbcl examples\GuessNumber.hbcl.txt
